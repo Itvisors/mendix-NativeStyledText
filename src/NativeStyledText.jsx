779595f470794d60682158eab96607745440d964
Marcel Groeneweg <mgroeneweg@users.noreply.github.com>
2020-12-08 13:48:47 +0100

    First version

--- src/NativeStyledText.jsx
+++ src/NativeStyledText.jsx
@@ -1,9 +1,49 @@
 import { Component, createElement } from "react";
+import { Text, View } from "react-native";
+import { mergeNativeStyles } from "@mendix/pluggable-widgets-tools";
 
-import { HelloWorld } from "./components/HelloWorld";
+const defaultStyle = {
+    container: {},
+    defaultTextStyle: {
+        color: "green"
+    }
+};
 
 export class NativeStyledText extends Component {
+    styles = mergeNativeStyles(defaultStyle, this.props.style);
     render() {
-        return <HelloWorld name={this.props.yourName} style={this.props.style} />;
+        return (
+            <View style={this.styles.container}>
+                <Text style={this.styles.defaultTextStyle}>{this.renderTexts()}</Text>
+            </View>
+        );
+    }
+
+    renderTexts() {
+        const { style, textList } = this.props;
+
+        if (!textList || textList.length === 0) {
+            return null;
+        }
+
+        let customStyle;
+        if (style && style.length) {
+            customStyle = style[0];
+        }
+
+        return textList.map((textItem, index) => {
+            const { textValue, textClass } = textItem;
+
+            if (!textValue || textValue.status !== "available") {
+                return null;
+            }
+            const textClassValue = textClass ? textClass.value : null;
+
+            return (
+                <Text key={index} style={customStyle[textClassValue]}>
+                    {textValue.value}
+                </Text>
+            );
+        });
     }
 }
---
+++ test/theme/styles/native/app/nativeStyledText.js
@@ -0,0 +1,15 @@
+import { brand, font } from "./custom-variables";
+
+export const nextAppointmentStyledText = {
+    container: {},
+    defaultTextStyle: {
+        color: font.color,
+        fontSize: font.size
+    },
+    nameStyle: {
+        color: brand.success
+    },
+    appointmentTime: {
+        color: brand.primary
+    }
+};
